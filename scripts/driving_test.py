1fe6ac455d0c6b2e56f2027e013a90a452f7a02e
seholm <filipslottner@gmail.com>
2017-02-15 20:21:22 +0100

    added test stubs

---
+++ scripts/driving_test.py
@@ -0,0 +1,12 @@
+import unittest
+#from driving import *
+
+class TestDriving(unittest.TestCase):
+    def test_drive_method1(self):
+        self.assertTrue(True)
+
+    def test_drive_method2(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
